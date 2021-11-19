b31052e8154bf139156998b9b3035fa3c9708fdf
EnzoAguero <agueroenzo10@gmail.com>
2021-11-19 16:06:15 -0300

    finalizando tp

---
+++ src/controllers/api/movieApi.js
@@ -0,0 +1,111 @@
+const path = require('path');
+const db = require('../../database/models');
+
+module.exports = {
+    create: (req,res) => {
+        Movies
+        .create(
+            {
+                title: req.body.title,
+                rating: req.body.rating,
+                awards: req.body.awards,
+                release_date: req.body.release_date,
+                length: req.body.length,
+                genre_id: req.body.genre_id
+            }
+        )
+        .then(confirm => {
+            let respuesta;
+            if(confirm){
+                respuesta ={
+                    meta: {
+                        status: 200,
+                        total: confirm.length,
+                        url: 'api/movies/create'
+                    },
+                    data:confirm
+                }
+            }else{
+                respuesta ={
+                    meta: {
+                        status: 200,
+                        total: confirm.length,
+                        url: 'api/movies/create'
+                    },
+                    data:confirm
+                }
+            }
+            res.json(respuesta);
+        })    
+        .catch(error => res.send(error))
+    },
+    update: (req,res) => {
+        let movieId = req.params.id;
+        Movies.update(
+            {
+                title: req.body.title,
+                rating: req.body.rating,
+                awards: req.body.awards,
+                release_date: req.body.release_date,
+                length: req.body.length,
+                genre_id: req.body.genre_id
+            },
+            {
+                where: {id: movieId}
+        })
+        .then(confirm => {
+            let respuesta;
+            if(confirm){
+                respuesta ={
+                    meta: {
+                        status: 200,
+                        total: confirm.length,
+                        url: 'api/movies/update/:id'
+                    },
+                    data:confirm
+                }
+            }else{
+                respuesta ={
+                    meta: {
+                        status: 204,
+                        total: confirm.length,
+                        url: 'api/movies/update/:id'
+                    },
+                    data:confirm
+                }
+            }
+            res.json(respuesta);
+        })    
+        .catch(error => res.send(error))
+    },
+    destroy: (req,res) => {
+        let movieId = req.params.id;
+        Movies
+        .destroy({where: {id: movieId}, force: true}) // force: true es para asegurar que se ejecute la acción
+        .then(confirm => {
+            let respuesta;
+            if(confirm){
+                respuesta ={
+                    meta: {
+                        status: 200,
+                        total: confirm.length,
+                        url: 'api/movies/destroy/:id'
+                    },
+                    data:confirm
+                }
+            }else{
+                respuesta ={
+                    meta: {
+                        status: 204,
+                        total: confirm.length,
+                        url: 'api/movies/destroy/:id'
+                    },
+                    data:confirm
+                }
+            }
+            res.json(respuesta);
+        })    
+        .catch(error => res.send(error))
+    }
+    
+}
